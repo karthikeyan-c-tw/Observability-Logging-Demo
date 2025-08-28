472fee5274c1ada48e749ae20b28705b22568dd5
Karthikeyan <karthikeyan.c@thoughtworks.com>
2025-08-28 13:42:16 +0530

    adding elk

--- example/src/main/java/com/prometheus/example/ExampleApplication.java
+++ example/src/main/java/com/prometheus/example/ExampleApplication.java
@@ -1,9 +1,11 @@
 package com.prometheus.example;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class ExampleApplication {
 
 	public static void main(String[] args) {
--- example/src/main/java/com/prometheus/example/controller/HelloController.java
+++ example/src/main/java/com/prometheus/example/controller/HelloController.java
@@ -1,14 +1,21 @@
 package com.prometheus.example.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HelloController {
 
     @GetMapping("/message/{msg}")
     public String hello(@PathVariable(name="msg") String message) {
+        log.info("message: {}", message);
+        if("hello".equalsIgnoreCase(message)) {
+            log.error("message is invalid");
+            return null;
+        }
         return "Hello! " + message;
     }
 }
